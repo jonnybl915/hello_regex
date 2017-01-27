26b961411fe2a92037221c497c46f5c4edd4f074
jon black <jonnybl915@gmail.com>
2017-01-27 15:21:29 -0500

    added initial file structure

---
+++ features/regex.feature
@@ -0,0 +1,6 @@
+Feature: Regex
+
+  Scenario: Email Form Validation
+    Given I have the email address qa@boomtownroi.com
+    When I validate the email address
+    Then I expect it to be valid
---
+++ features/step_definitions/regex_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I have the email address \w+@\w+\.com$/) do |email|
+  @email = email
+end
+
+When(/^I validate the email address$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I expect it to be valid$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
