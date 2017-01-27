74aa82c782b605fef4181f17c8131c5acaa7d684
jon black <jonnybl915@gmail.com>
2017-01-27 16:09:17 -0500

    built up step to test both validity and invalidity

--- features/regex.feature
+++ features/regex.feature
@@ -4,3 +4,8 @@ Feature: Regex
     Given I have the email address qa@boomtownroi.com
     When I validate the email address
     Then I expect it to be valid
+
+  Scenario: Email Form Validation
+    Given I have the email @google.com
+    When I validate the email address
+    Then I expect it to be invalid
--- features/step_definitions/regex_steps.rb
+++ features/step_definitions/regex_steps.rb
@@ -1,11 +1,16 @@
-Given(/^I have the email address \w+@\w+\.com$/) do |email|
+Given(/^I have the email address (.*gmail)$/) do |email|
   @email = email
 end
 
 When(/^I validate the email address$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  email_regex = /^\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  @regex_comparison_result = expect(email_regex.match(@email))
 end
 
-Then(/^I expect it to be valid$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^I expect it to be (valid|invalid)$/) do |validity|
+  if validity == 'valid'
+    expect(@regex_comparison_result).not_to be_nil
+  else
+    expect(@regex_comparison_result).to eq nil
+  end
 end
